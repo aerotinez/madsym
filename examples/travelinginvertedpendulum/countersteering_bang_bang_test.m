0b3f6f9471559a364957a6333e4754d505cf3d68
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2024-01-15 17:23:21 +0100

    Fix cumulative sum calculation in countersteering_bang_bang_test.m

--- examples/travelinginvertedpendulum/countersteering_bang_bang_test.m
+++ examples/travelinginvertedpendulum/countersteering_bang_bang_test.m
@@ -1,10 +1,9 @@
 setmadsympath();
 close("all"); clear; clc;
 
-ts = 1/120;
 u = [-ones(1,100),ones(1,300),-ones(1,200),-ones(1,200),ones(1,300),-ones(1,100)];
-v = ts.*cumsum(u);
-p = ts.*cumsum(v);
+v = cumsum(u);
+p = cumsum(v);
 
 hold("on");
 plot(u./max(u));
