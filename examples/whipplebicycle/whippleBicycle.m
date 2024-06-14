a16dbdbf422c20b64b3c64829ba1459cf87b69b5
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2024-06-14 18:40:55 +0200

    chore: Update extendedSharpMotorcycleLinearizedInputMatrix and extendedSharpMotorcycleLinearizedForcingMatrix functions

--- examples/whipplebicycle/whippleBicycle.m
+++
@@ -1,8 +0,0 @@
-function x_dot = whippleBicycle(x,u,p)
-%    states = [psi varphi theta_r x y theta x_f y_f psi_f varphi_f theta_f beta u_1 u_2 u_3]
-%    inputs = [M_th M_delta]
-%    params = [I_bx I_bxz I_by I_bz I_fx I_fy I_fz I_hx I_hxz I_hy I_hz I_rx I_ry I_rz b d e f g h m_b m_f m_h m_r p r_f r_r varepsilon]
-M = whippleBicycleMassMatrix(x,u,p);
-f = whippleBicycleForcingVector(x,u,p);
-x_dot = M\f;
-end
