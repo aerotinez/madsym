70752fe6230f22c8623a81dac80d6b18a47ee828
Martin Pryde <martinpryde@ymail.com>
2024-08-28 21:47:24 +0200

    Refactor linearize function to simplify code and improve readability

--- linearization/@LinearizedMotionEquations/LinearizedMotionEquations.m
+++ linearization/@LinearizedMotionEquations/LinearizedMotionEquations.m
@@ -1,4 +1,4 @@
-classdef LinearizedMotionEquations < handle 
+classdef LinearizedMotionEquations 
     properties (GetAccess = public, SetAccess = private)
         States;
         Rates;
@@ -6,34 +6,28 @@ classdef LinearizedMotionEquations < handle
         MassMatrix;
         ForcingMatrix;
         InputMatrix;
-        PermutationMatrix;
     end
     methods (Access = public)
         function obj = LinearizedMotionEquations( ...
             states, ...
             mass_matrix, ...
             forcing_matrix, ...
-            inputs, ...
             input_matrix, ...
-            permutation_matrix ...
+            inputs ...
             )
             arguments
                 states sym;
                 mass_matrix sym;
                 forcing_matrix sym;
-                inputs sym = sym.empty(0,1);
                 input_matrix sym = sym.empty(numel(states),0);
-                permutation_matrix sym = sym.empty(0,0);
+                inputs sym = sym.empty(0,1);
             end
             obj.States = states;
-            obj.validateStates();
             obj.Rates = diff(obj.States);
             obj.MassMatrix = mass_matrix;
             obj.ForcingMatrix = forcing_matrix;
             obj.Inputs = inputs;
-            obj.validateInputs();
             obj.InputMatrix = input_matrix;
-            obj.PermutationMatrix = permutation_matrix;
         end 
     end 
 end
