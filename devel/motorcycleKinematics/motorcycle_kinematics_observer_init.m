04d0d15498e69f209b8fadd22cbaf2329930c79e
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2024-09-09 16:53:45 +0200

    Refactor motorcycle kinematics observer initialization code

--- devel/motorcycleKinematics/motorcycle_kinematics_observer_init.m
+++ devel/motorcycleKinematics/motorcycle_kinematics_observer_init.m
@@ -13,29 +13,25 @@ tf = t(end);
 %% Geometric parameters
 params = [
     0.7525;
+    0.0882;
     0.6429;
     0.4189;
     0.0253;
     0.2344;
-    0.5750
+    0.5750;
+    0.282;
+    0.297;
     ];
 
 %% Generalized coordinates
 yaw = T.Yaw;
 pitch = T.Pitch;
 roll = T.Roll_E;
+[yaw,lean,pitch] = eulZYXToZXY(yaw,pitch,roll);
 steer = T.Steer;
-eul = [yaw,pitch,roll,steer];
+eul = [yaw,lean,pitch,steer];
 q = timeseries(deg2rad(eul),t);
 q0 = q.Data(1,:);
-
-%% Generalized speeds
-yaw_rate = T.AV_Y;
-pitch_rate = T.AV_P;
-roll_rate = T.AV_R;
-steer_rate = T.M_StrSys./0.2122;
-euld = [yaw_rate,pitch_rate,roll_rate,steer_rate];
-qd = timeseries(deg2rad(euld),t);
 
 %% Steering frame velocities
 wx = T.AVx;
@@ -52,5 +48,5 @@ u = timeseries([deg2rad(w),v./3.6],t);
 nx = 4;
 nz = 9;
 P0 = eye(nx);
-Q = 1E-03.*eye(nx);
+Q = eye(nx);
 R = eye(nz);
