d2c9798c623ee21e9b7221624dbb767b46dd91ff
Martin Pryde <martinpryde@ymail.com>
2024-09-23 21:20:32 +0200

    Reorder scatter plot legend in compare_lagrange_gibbs_appell_results.m

--- examples/sharpmotorcycle/compare_lagrange_gibbs_appell_results.m
+++ examples/sharpmotorcycle/compare_lagrange_gibbs_appell_results.m
@@ -3,23 +3,23 @@ setmadsympath();
 load("poles_lagrange");
 load("poles_gibbs_appell");
 
-xl = sort(poles_lagrange(:));
-xg = sort(poles_gibbs_appell(:));
+xg = sort(poles_lagrange(:));
+xl = sort(poles_gibbs_appell(:));
 
 figure();
 hold("on");
 c = matplotlibcolors;
-scatter(real(xg),imag(xg),'filled','SizeData',50,"CData",[0,0,0]);
-scatter(real(xl),imag(xl),'filled','SizeData',5,"CData",[1,1,1]);
+scatter(real(xg),imag(xg),'filled','SizeData',100);
+scatter(real(xl),imag(xl),'filled','SizeData',10);
 hold("off");
 axe = gca;
 % set(axe,'Color','k')
 box("on");
 grid("on");
 % axe.GridColor = ones(1,3);
-xlim([-252,10]);
+xlim([-255,10]);
 ylim([-63,63]);
 xlabel("Re");
 ylabel("Im");
-leg = legend("Gibbs-Appell","Lagrange","Location","northwest");
+leg = legend("Lagrange","Gibbs-Appell","Location","northwest");
 set(leg,'color',ones(1,3));
