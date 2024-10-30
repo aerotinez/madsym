2baa609278693e0d96d0f051399dce1406de1f88
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2024-10-30 17:33:19 +0100

    Remove unused PrydeMotorcycleParameters class and related XML files

--- linearization/@LinearizedMotionEquations/matlabFunction.m
+++ linearization/@LinearizedMotionEquations/matlabFunction.m
@@ -4,39 +4,47 @@ function matlabFunction(obj,name)
         name (1,1) string
     end
     eom = obj;
-    if ~isequal(obj.MassMatrix,eye(numel(obj.States)))
-        eom = toODE(obj);
-    end
+    
     x = prettify(eom.States.state);
     u = prettify(eom.Inputs.state);
-    A = prettify(eom.ForcingMatrix);
-    B = prettify(eom.InputMatrix);
-    p = setdiff(symvar([A,B]),x).';
+    P = permMatInd(eom.States);
+    M = prettify(eom.MassMatrix);
+    H = prettify(eom.ForcingMatrix);
+    G = prettify(eom.InputMatrix);
+    p = setdiff(symvar([M,H,G]),x).';
 
     dir_name = name + "StateSpace";
     if ~exist(dir_name, 'dir')
         mkdir(dir_name)
     end
 
-    Astr = dir_name + "/" + name + "StateMatrix";
-    Bstr = dir_name + "/" + name + "InputMatrix";
+    Pstr = dir_name + "/" + name + "PermutationMatrix";
+    Mstr = dir_name + "/" + name + "MassMatrix";
+    Hstr = dir_name + "/" + name + "ForcingMatrix";
+    Gstr = dir_name + "/" + name + "InputMatrix";
 
     state_str = "   states = [" + strjoin(string(x).') + "]";
     input_str = "   inputs = [" + strjoin(string(u).') + "]";
     param_str = "   params = [" + strjoin(string(p).') + "]";
     comment_str = [state_str,input_str,param_str,""].';
 
-    vars = {x,p};
-    matlabFunction(A,"File",Astr,"Vars",vars,"Comments",comment_str);
-    matlabFunction(B,"File",Bstr,"Vars",vars,"Comments",comment_str);
+    vars = {p};
+    matlabFunction(P,"File",Pstr,"Comments",comment_str);
+    matlabFunction(M,"File",Mstr,"Vars",vars,"Comments",comment_str);
+    matlabFunction(H,"File",Hstr,"Vars",vars,"Comments",comment_str);
+    matlabFunction(G,"File",Gstr,"Vars",vars,"Comments",comment_str);
 
-    fid = fopen(name + "StateSpace.m","w");
-    fprintf(fid,"function sys = %sStateSpace(x,p)\n",name);
+    fid = fopen(dir_name + "/" + name + "StateSpace.m","w");
+    fprintf(fid,"function sys = %sStateSpace(p)\n",name);
     fprintf(fid,"   %% %s\n",state_str);
     fprintf(fid,"   %% %s\n",input_str);
     fprintf(fid,"   %% %s\n",param_str);
-    fprintf(fid,"   A = %sStateMatrix(x,p);\n",name);
-    fprintf(fid,"   B = %sInputMatrix(x,p);\n",name);
+    fprintf(fid,"   P = %sPermutationMatrix();\n",name);
+    fprintf(fid,"   M = %sMassMatrix(p);\n",name);
+    fprintf(fid,"   H = %sForcingMatrix(p);\n",name);
+    fprintf(fid,"   G = %sInputMatrix(p);\n",name);
+    fprintf(fid,"   A = P*(M\\H);\n");
+    fprintf(fid,"   B = P*(M\\G);\n");
     fprintf(fid,"   sys = ss(A,B,eye(size(A)),0);\n");
     fprintf(fid,"end\n");
     fclose(fid);
