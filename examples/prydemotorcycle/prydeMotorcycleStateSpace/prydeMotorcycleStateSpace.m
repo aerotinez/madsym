2e7b5e2d6857953b56ad0a8bba1ef3d22d478528
Martin Pryde <martinpryde@ymail.com>
2024-12-04 00:11:35 +0100

    Update Pryde motorcycle state space functions to include state, input, and output names; update generated timestamps

--- examples/prydemotorcycle/prydeMotorcycleStateSpace/prydeMotorcycleStateSpace.m
+++ examples/prydemotorcycle/prydeMotorcycleStateSpace/prydeMotorcycleStateSpace.m
@@ -9,4 +9,29 @@ function sys = prydeMotorcycleStateSpace(p)
    A = P*(M\H);
    B = P*(M\G);
    sys = ss(A,B,eye(size(A)),0);
+
+   sys.StateName = [
+      "varphi"
+      "delta"
+      "omega_bz"
+      "v_rx"
+      "v_ry"
+      "omega_bx"
+      "omega_delta"
+   ];
+
+   sys.InputName = [
+      "tau_delta"
+   ];
+
+   sys.OutputName = [
+      "varphi"
+      "delta"
+      "omega_bz"
+      "v_rx"
+      "v_ry"
+      "omega_bx"
+      "omega_delta"
+   ];
+
 end
