344540a4b50fe9164a07a4e7b21312a1a651d590
Martin Pryde <martinpryde@ymail.com>
2024-12-08 14:51:49 +0100

    Add selector matrix and update state space functions for Pryde motorcycle model

--- examples/prydemotorcycle/prydeMotorcycleStateSpace/prydeMotorcycleStateSpace.m
+++ examples/prydemotorcycle/prydeMotorcycleStateSpace/prydeMotorcycleStateSpace.m
@@ -2,71 +2,42 @@ function sys = prydeMotorcycleStateSpace(p)
    %    states = [varphi delta omega_bz v_rx v_ry omega_bx omega_r omega_delta omega_f]
    %    inputs = [tau_r tau_delta]
    %    params = [C_delta I_bxx I_bxz I_bzz I_hxx I_hzz V Z_f Z_r a a_f0 a_n a_r0 b e f f_w0 f_w2 g h i_fy i_ry k_alpha_f k_alpha_r k_kappa_f k_kappa_r k_varphi_f k_varphi_r m_b m_h rho_f rho_r t_f t_r varepsilon]
+   S = prydeMotorcycleSelectorMatrix();
    P = prydeMotorcyclePermutationMatrix();
    M = prydeMotorcycleMassMatrix(p);
    H = prydeMotorcycleForcingMatrix(p);
    G = prydeMotorcycleInputMatrix(p);
-   A = P*(M\H);
-   B = P*(M\G);
+   A = S*P*(M\H)*S.';
+   B = S*P*(M\G);
    sys = ss(A,B,eye(size(A)),0);
 
    sys.StateName = [
-      "psi"
-      "p_x"
-      "p_y"
-      "varphi"
-      "theta_r"
-      "delta"
-      "theta_f"
-      "theta"
-      "psi_f"
-      "p_fx"
-      "p_fy"
-      "varphi_f"
-      "omega_bz"
-      "v_rx"
-      "v_ry"
-      "omega_bx"
-      "omega_r"
-      "omega_delta"
-      "omega_f"
-      "omega_by"
-      "omega_fz"
-      "v_fx"
-      "v_fy"
-      "omega_fx"
+      "varphi";
+      "delta";
+      "omega_bz";
+      "v_rx";
+      "v_ry";
+      "omega_bx";
+      "omega_r";
+      "omega_delta";
+      "omega_f";
    ];
 
    sys.InputName = [
-      "tau_r"
-      "tau_delta"
+      "tau_r";
+      "tau_delta";
    ];
 
    sys.OutputName = [
-      "psi"
-      "p_x"
-      "p_y"
-      "varphi"
-      "theta_r"
-      "delta"
-      "theta_f"
-      "theta"
-      "psi_f"
-      "p_fx"
-      "p_fy"
-      "varphi_f"
-      "omega_bz"
-      "v_rx"
-      "v_ry"
-      "omega_bx"
-      "omega_r"
-      "omega_delta"
-      "omega_f"
-      "omega_by"
-      "omega_fz"
-      "v_fx"
-      "v_fy"
-      "omega_fx"
+      "varphi";
+      "delta";
+      "omega_bz";
+      "v_rx";
+      "v_ry";
+      "omega_bx";
+      "omega_r";
+      "omega_delta";
+      "omega_f";
    ];
 
 end
--- linearization/@LinearizedMotionEquations/matlabFunction.m
+++ linearization/@LinearizedMotionEquations/matlabFunction.m
@@ -13,7 +13,7 @@ function matlabFunction(obj,name,states)
     x = prettify(states.state);
     u = prettify(eom.Inputs.state);
     P = eom.States.permMatInd;
-    Pr = jacobian(states.state,eom.States.independent.state);
+    S = jacobian(states.state,eom.States.independent.state);
     M = prettify(eom.MassMatrix);
     H = prettify(eom.ForcingMatrix);
     G = prettify(eom.InputMatrix);
@@ -24,6 +24,7 @@ function matlabFunction(obj,name,states)
         mkdir(dir_name)
     end
 
+    Sstr = dir_name + "/" + name + "SelectorMatrix";
     Pstr = dir_name + "/" + name + "PermutationMatrix";
     Mstr = dir_name + "/" + name + "MassMatrix";
     Hstr = dir_name + "/" + name + "ForcingMatrix";
@@ -35,13 +36,14 @@ function matlabFunction(obj,name,states)
     comment_str = [state_str,input_str,param_str,""].';
 
     vars = {p};
-    matlabFunction(Pr*P,"File",Pstr,"Comments",comment_str);
+    matlabFunction(S,"File",Sstr,"Comments",comment_str);
+    matlabFunction(P,"File",Pstr,"Comments",comment_str);
     matlabFunction(M,"File",Mstr,"Vars",vars,"Comments",comment_str);
     matlabFunction(H,"File",Hstr,"Vars",vars,"Comments",comment_str);
     matlabFunction(G,"File",Gstr,"Vars",vars,"Comments",comment_str);
 
     f = @(x)arrayfun(@(y)strrep(string(y.state),"(t)",""),x);
-    state_names = f(obj.States);
+    state_names = f(states);
     input_names = f(obj.Inputs);
 
     fid = fopen(dir_name + "/" + name + "StateSpace.m","w");
@@ -49,21 +51,22 @@ function matlabFunction(obj,name,states)
     fprintf(fid,"   %% %s\n",state_str);
     fprintf(fid,"   %% %s\n",input_str);
     fprintf(fid,"   %% %s\n",param_str);
+    fprintf(fid,"   S = %sSelectorMatrix();\n",name);
     fprintf(fid,"   P = %sPermutationMatrix();\n",name);
     fprintf(fid,"   M = %sMassMatrix(p);\n",name);
     fprintf(fid,"   H = %sForcingMatrix(p);\n",name);
     fprintf(fid,"   G = %sInputMatrix(p);\n",name);
-    fprintf(fid,"   A = P*(M\\H);\n");
-    fprintf(fid,"   B = P*(M\\G);\n");
+    fprintf(fid,"   A = S*P*(M\\H)*S.';\n");
+    fprintf(fid,"   B = S*P*(M\\G);\n");
     fprintf(fid,"   sys = ss(A,B,eye(size(A)),0);\n\n");
     fprintf(fid,"   sys.StateName = [\n");
-    arrayfun(@(x)fprintf(fid,"      """ + x + """\n"),state_names);
+    arrayfun(@(x)fprintf(fid,"      """ + x + """;\n"),state_names);
     fprintf(fid,"   ];\n\n");
     fprintf(fid,"   sys.InputName = [\n");
-    arrayfun(@(x)fprintf(fid,"      """ + x + """\n"),input_names);
+    arrayfun(@(x)fprintf(fid,"      """ + x + """;\n"),input_names);
     fprintf(fid,"   ];\n\n");
     fprintf(fid,"   sys.OutputName = [\n");
-    arrayfun(@(x)fprintf(fid,"      """ + x + """\n"),state_names);
+    arrayfun(@(x)fprintf(fid,"      """ + x + """;\n"),state_names);
     fprintf(fid,"   ];\n\n");
     fprintf(fid,"end\n");
     fclose(fid);
