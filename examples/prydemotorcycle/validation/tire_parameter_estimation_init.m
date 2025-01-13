374c612b38766a7fa800276210b10275e75a1480
Martin Pryde <martinpryde@ymail.com>
2025-01-13 12:47:43 +0100

    Add cache files and update tire parameter estimation initialization

--- examples/prydemotorcycle/validation/tire_parameter_estimation_init.m
+++ examples/prydemotorcycle/validation/tire_parameter_estimation_init.m
@@ -6,7 +6,10 @@ n2s = @num2str;
 results_path = "G:\My Drive\BikeSimResults\BigSports\OpenLoop";
 speed_path = "\Vx" + n2s(vx) + "Kph\";
 file_name = "bikesim_results_" + n2s(vx) + "kph.csv";
-results = readtable(results_path + speed_path + file_name);
+% results = readtable(results_path + speed_path + file_name);
+% load("G:\My Drive\BikeSimResults\BigSports\Slalom50Kph\results_slalom_50kph.mat");
+results = readtable("G:\My Drive\BikeSimResults\BigSports\ICRA25\Vx50Kph\bikesim_results_50kph.csv");
+
 
 %% Signal data
 time = results.Time;
@@ -18,6 +21,11 @@ wx = timeseries(results.AVx,time);
 ws = timeseries(-results.M_StrSys./0.2212,time);
 
 Mz = timeseries(results.M_Str_In,time);
+
+%% Initial condition
+x = [camber.Data,steer.Data,wz.Data,vy.Data,wx.Data,ws.Data];
+s = (pi/180).*[1,1,1,0,1,1] + [0,0,0,1,0,0];
+IC = s.*x(1,:);
 
 %% Initial parameters
 bs = bigSportsParameters;
@@ -31,11 +39,11 @@ params = s2m(bikeSimToPrydeParameters(bs,vx/3.6));
 % kfyar = params(13);
 % kfylf = params(14);
 % kfylr = params(15);
-kfyaf = 27.858;
-kfyar = 9.5992;
-kfylf = 0.0086564;
-kfylr = 0.03315;
-kmzaf = 0.68051;
-kmzar = 4.9337;
-kmzlf = 0.062821;
-kmzlr = 0.064109;
+kfyaf = 31.174;
+kfyar = 13.794;
+kfylf = 0.00061688;
+kfylr = 0.034478;
+kmzaf = 0.69133;
+kmzar = 7.3564;
+kmzlf = 0.027474;
+kmzlr = 0.4764;
