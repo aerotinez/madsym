87837a84902273c7ea7ce721893dff86b4d53c22
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2025-03-26 13:48:40 +0100

    Update lateral observer files and regenerate UI state space matrices

--- examples/prydemotorcycle/observers/lateral_observer_init.m
+++ examples/prydemotorcycle/observers/lateral_observer_init.m
@@ -4,8 +4,6 @@ setmadsympath();
 %% Parameters
 bs = bigSportsParameters;
 s2m = @(x)cell2mat(struct2cell(x));
-disp(bikeSimToPrydeParameters(bs,30/3.6));
-params = @(v)s2m(bikeSimToPrydeParameters(bs,v/3.6));
 ts = 1/60;
 
 %% BikeSim results
@@ -14,7 +12,7 @@ plant = @prydeMotorcycleUILateralStateSpace;
 n2s = @num2str;
 results_path = "G:\My Drive\BikeSimResults\BigSports\Chicane";
 
-k = 4;
+k = 2;
 
 speed_path = "\Vx" + n2s(vx(k)) + "Kph\";
 file_name = "bikesim_results_" + n2s(vx(k)) + "kph.csv";
@@ -32,15 +30,17 @@ ws = timeseries(deg2rad(-results.M_StrSys./0.2212),time);
 x = [camber,steer,wz,vy,wx,ws];
 x0 = arrayfun(@(x)x.Data(1),x);
 
+Mx = timeseries(results.Mx_Rider,time);
 Mz = timeseries(results.M_Str_In,time);
-p = params(vx(k));
-sys = plant(p);
+Fl = timeseries(results.Fy_Rider,time);
+p = bikeSimToPrydeParameters(bs,vx(k)/3.6);
+sys = plant(s2m(p));
 
 %% Plant matrices
 
 A = sys.A;
 B = sys.B(:,2);
-E = sys.B(:,3:6);
+E = sys.B(:,[7,8]);
 
 C = [
     1,0,0,0,0,0;
@@ -53,12 +53,3 @@ nx = size(A,1);
 nu = size(B,2);
 nd = size(E,2);
 ny = size(C,1);
-
-% check that a UIO can be designed
-if rank(C*E) ~= rank(E)
-    error("No unknown input observer exists for this system")
-end
-
-%% Design decoupling matrix
-I= eye(nx);
-[H,T,g,sol] = findDecouplingMatrix(C,E);
