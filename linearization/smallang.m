fe51b4b9e4dfd69c4f89983e80c97714bd9be1da
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2026-04-28 16:13:45 +0200

    28/04/2026

--- linearization/smallang.m
+++ linearization/smallang.m
@@ -10,18 +10,24 @@ function f0 = smallang(f, angs)
     f0 = expand(f0);
 
     % First-order trig substitutions
-    f0 = subs(f0, sin(x), x);
-    f0 = subs(f0, cos(x), sym(ones(size(x))));
+    f0 = subs(f0,cos(x),sym(ones(size(x))));
+    f0 = subs(f0,sin(x),x);
 
     % Expand again so newly created products are visible
     f0 = expand(f0);
 
     % Remove all quadratic and higher terms in the chosen small angles
-    for i = 1:numel(x)
-        for j = i:numel(x)
-            f0 = subs(f0, x(i)*x(j), sym(0));
-        end
-    end
+    prods = triu(x*x.',1);
 
-    f0 = simplify(f0, 20);
+    x0 = [
+        nonzeros(prods(:));
+        x.^2;
+        x.^3;
+        x.^4;
+        x.^5
+        ];
+
+    f0 = subs(f0,x0,zeros(size(x0)));
+    f0 = simplify(collect(f0,x));
+    f0 = subs(f0,x0,zeros(size(x0)));
 end
