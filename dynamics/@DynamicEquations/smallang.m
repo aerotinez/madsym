9d71b987049238e260853cc38c9e56b950cb06fc
Martin Pryde <martinpryde@ymail.com>
2026-05-01 00:15:37 +0200

    01/05/2026

--- dynamics/@DynamicEquations/smallang.m
+++ dynamics/@DynamicEquations/smallang.m
@@ -12,6 +12,21 @@ function out = smallang(obj,x)
     out.ActiveForces = smallang(obj.ActiveForces,x);
     out.ConstraintJacobian = smallang(obj.ConstraintJacobian,x);
 
-    out.MassMatrix = out.massMatrix();
-    out.ForcingVector = out.forcingVector();
+    vals = [
+        nonzeros(triu(x*x.',1));
+        x.^2;
+        x.^3;
+        x.^4;
+        x.^5;
+        x.^6;
+        x.^7;
+        x.^8;
+        x.^9;
+        x.^10;
+        x.^11;
+        x.^12
+        ];
+
+    out.MassMatrix = subs(expand(out.massMatrix()),vals,0*vals);
+    out.ForcingVector = subs(expand(out.forcingVector()),vals,0*vals);
 end
