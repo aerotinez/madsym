fc061a8661988f8d1feda9fed2e468053b79382a
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2026-06-01 12:02:06 +0200

    01/06/2026

--- foundation/@MotionEquations/sym.m
+++ foundation/@MotionEquations/sym.m
@@ -1,10 +1,17 @@
-function eqns = sym(obj)
+function eqns = sym(obj,options)
     arguments
         obj (1,1) MotionEquations;
+        options.SmallAng (:,1) = sym.empty(0,1);
     end
+
+    f = @(x)x;
+    if ~isempty(options.SmallAng)
+        f = @(x)smallang(x,options.SmallAng);
+    end
+
     if obj.IsTrimmed
-        eqns = obj.MassMatrix*[obj.States.TrimRate].' - obj.ForcingVector;
+        eqns = f(obj.MassMatrix*[obj.States.TrimRate].') - obj.ForcingVector;
         return
     end
-    eqns = obj.MassMatrix*obj.States.rate() - obj.ForcingVector;
+    eqns = f(obj.MassMatrix*obj.States.rate()) - obj.ForcingVector;
 end
