17c81ffab4fa197198453c5f1ce0179c47290029
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2026-06-18 16:26:00 +0200

    18/06/2026

--- examples/rrarm/rr_arm_control_init.m
+++ examples/rrarm/rr_arm_control_init.m
@@ -18,3 +18,24 @@ l2 = 1;
 b2 = 0.1;
 
 params = [Iyy1,Iyy2,m1,m2,l1,l2,g].';
+
+S1 = rscrew('y',[0,0,0]);
+S2 = rscrew('y',[l1,0,0]);
+
+function s = jointAxis(axis)
+arguments
+    axis (1,1) char {mustBeMember(axis,'xyzXYZ')};
+end
+I = eye(3);
+s = I(:,lower(axis) == 'xyz');
+end
+
+function S = rscrew(axis,position,orientation)
+arguments
+    axis;
+    position (3,1) double;
+    orientation (3,3) double = eye(3);
+end
+w = orientation*jointAxis(axis);
+S = [w.',cross(position,w).'].';
+end
