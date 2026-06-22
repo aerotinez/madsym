1211cb2dcbdebe81e9d21c0f6c0666be499354bd
Martin Pryde <martinpryde@ymail.com>
2026-06-22 11:37:14 +0200

    22/06/2026

--- examples/rrarm/rr_arm_control_init.m
+++ examples/rrarm/rr_arm_control_init.m
@@ -7,12 +7,12 @@ Ixx1 = 1;
 Iyy1 = 1;
 Izz1 = 1;
 m1 = 1;
-b1 = 0;
+b1 = 0.9;
 G1 = blkdiag(diag([Ixx1,Iyy1,Izz1]),m1*eye(3));
 l1 = 1;
 
 M1 = [
-    eye(3),-[0,0,l1/2]';
+    eye(3),[l1/2,0,0]';
     0,0,0,1
     ];
 
@@ -20,19 +20,19 @@ Ixx2 = 1;
 Iyy2 = 1;
 Izz2 = 1;
 m2 = 1;
-b2 = 0;
+b2 = 0.9;
 G2 = blkdiag(diag([Ixx2,Iyy2,Izz2]),m2*eye(3));
 l2 = 1;
 
 M2 = [
-    eye(3),-[0,0,l1 + l2/2]';
+    eye(3),[l1 + l2/2,0,0]';
     0,0,0,1
     ];
 
-params = [Iyy1,Iyy2,m1,m2,l1,l2,g].';
+params = [Iyy1,Iyy2,m1,m2,l1,l2,b1,b2,g].';
 
 S1 = rscrew('y',[0,0,0]);
-S2 = rscrew('y',[0,0,-l1]);
+S2 = rscrew('y',[l1,0,0]);
 
 function s = jointAxis(axis)
 arguments
